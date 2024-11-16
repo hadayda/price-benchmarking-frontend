877b38f647124aea055dc769d60bd5df88dc4b49
Hadayda <salah.hadayda@gmail.com>
2024-11-16 12:17:50 +0200

    Deploying to Netfly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,6 @@ import {Link, useNavigate} from "react-router-dom";
 import {useContext} from "react";
 import {AuthContext} from "../AuthContext";
 
-const menuItems = [
-    {
-        label: <Link to="/dashboard" className="text-decoration-none">Dashboard</Link>,
-        key: '/dashboard',
-        icon: <DashboardOutlined/>
-    },
-    {
-        label: <Link to="/upload-rates" className="text-decoration-none">Upload Rates</Link>,
-        key: '/upload-rates',
-        icon: <FileOutlined/>
-    },
-    {
-        label: 'Logout',
-        key: 'logout',
-        icon: <LogoutOutlined/>
-    }
-];
-
 export default function Navbar({collapsed}) {
     const {logout} = useContext(AuthContext)
     const navigate = useNavigate();
@@ -29,6 +11,26 @@ export default function Navbar({collapsed}) {
         logout()
         navigate('/login');
     }
+
+    const menuItems = [
+        {
+            label: <Link to="/dashboard" className="text-decoration-none">Dashboard</Link>,
+            key: '/dashboard',
+            icon: <DashboardOutlined/>
+        },
+        {
+            label: <Link to="/upload-rates" className="text-decoration-none">Upload Rates</Link>,
+            key: '/upload-rates',
+            icon: <FileOutlined/>
+        },
+        {
+            label: 'Logout',
+            key: 'logout',
+            icon: <LogoutOutlined/>,
+            onClick: handleLogout
+        }
+    ];
+
     return <>
         <div className="logo text-center p-3 text-white">
             <h5 className="mb-0">{collapsed ? 'PB' : 'Price Benchmarking'}</h5>
